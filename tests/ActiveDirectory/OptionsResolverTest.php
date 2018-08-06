1a5466fd13400f5bd154441a7c150fcff5f3d490
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-08-07 00:46:40 +0200

    Adds first unit test :-)

---
+++ tests/ActiveDirectory/OptionsResolverTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Tests\ActiveDirectory;
+
+use App\ActiveDirectory\OptionResolver;
+use App\Entity\ActiveDirectoryGradeSyncOption;
+use App\Entity\ActiveDirectorySyncOption;
+use App\Entity\ActiveDirectorySyncSourceType;
+use App\Entity\UserType;
+use PHPUnit\Framework\TestCase;
+
+class OptionsResolverTest extends TestCase {
+    public function testResolveGradeByGroup() {
+        $options = [
+            (new ActiveDirectoryGradeSyncOption())->setSource('5A')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setGrade('5A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('6A')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setGrade('6A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('7A')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setGrade('7A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('8A')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setGrade('8A')
+        ];
+
+        $optionResolver = new OptionResolver();
+
+        $this->assertEquals($options[1], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ '6A' ]));
+        $this->assertEquals($options[1], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ 'Some-Group', '6A' ]));
+        $this->assertEquals($options[0], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ '5A', '6A' ]));
+    }
+
+    public function testResolveGradeByOU() {
+        $options = [
+            (new ActiveDirectoryGradeSyncOption())->setSource('OU=5A,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setGrade('5A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('OU=6A,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setGrade('6A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('OU=7A,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setGrade('7A'),
+            (new ActiveDirectoryGradeSyncOption())->setSource('OU=8A,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setGrade('8A')
+        ];
+
+        $optionResolver = new OptionResolver();
+
+        $this->assertEquals($options[1], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ '6A' ]));
+        $this->assertEquals($options[1], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ 'Some-Group', '6A' ]));
+        $this->assertEquals($options[1], $optionResolver->getOption($options, 'OU=6A,DC=test,DC=lokal', [ '5A' ]));
+    }
+
+    public function testResolveUserTypeByGroup() {
+        $userTypeTeacher = (new UserType())
+            ->setName('Teacher');
+
+        $userTypeStudent = (new UserType())
+            ->setName('Student');
+
+        $userTypeFoo = (new UserType())
+            ->setName('Foo');
+
+        $options = [
+            (new ActiveDirectorySyncOption())->setSource('Teachers')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setUserType($userTypeTeacher),
+            (new ActiveDirectorySyncOption())->setSource('Students')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setUserType($userTypeStudent),
+            (new ActiveDirectorySyncOption())->setSource('Foo')->setSourceType(ActiveDirectorySyncSourceType::GROUP)->setUserType($userTypeFoo)
+        ];
+
+        $optionResolver = new OptionResolver();
+
+        $this->assertEquals($userTypeTeacher, $optionResolver->getOption($options, 'OU=Teachers,DC=test,DC=lokal', ['Teachers', 'Some-Group'])->getUserType());
+        $this->assertEquals($userTypeStudent, $optionResolver->getOption($options, 'OU=Students,DC=test,DC=lokal', ['Students', 'Some-Group'])->getUserType());
+        $this->assertEquals($userTypeTeacher, $optionResolver->getOption($options, 'OU=Students,DC=test,DC=lokal', ['Teachers', 'Students', 'Some-Group'])->getUserType());
+        $this->assertNull($optionResolver->getOption($options, 'OU=Bla,DC=test,DC=lokal', [ ]));
+    }
+
+    public function testResolveUserTypeByOU() {
+        $userTypeTeacher = (new UserType())
+            ->setName('Teacher');
+
+        $userTypeStudent = (new UserType())
+            ->setName('Student');
+
+        $userTypeFoo = (new UserType())
+            ->setName('Foo');
+
+        $options = [
+            (new ActiveDirectorySyncOption())->setSource('OU=Teachers,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setUserType($userTypeTeacher),
+            (new ActiveDirectorySyncOption())->setSource('OU=Students,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setUserType($userTypeStudent),
+            (new ActiveDirectorySyncOption())->setSource('OU=Foo,DC=test,DC=lokal')->setSourceType(ActiveDirectorySyncSourceType::OU)->setUserType($userTypeFoo)
+        ];
+
+        $optionResolver = new OptionResolver();
+
+        $this->assertEquals($userTypeTeacher, $optionResolver->getOption($options, 'OU=Teachers,DC=test,DC=lokal', ['Teachers', 'Some-Group'])->getUserType());
+        $this->assertEquals($userTypeStudent, $optionResolver->getOption($options, 'OU=Students,DC=test,DC=lokal', ['Students', 'Some-Group'])->getUserType());
+        $this->assertNull($optionResolver->getOption($options, 'OU=Bla,DC=test,DC=lokal', [ ]));
+    }
+}
