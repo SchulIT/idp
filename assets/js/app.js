9d5e08b043bcff816c0bdf4917d50a9c9c4c4d81
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-08-12 15:20:29 +0200

    Make jQuery available as $

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,14 @@ require('../css/app.scss');
 
 var $ = require('jquery');
 require('bootstrap');
+
+// Make jQuery available
+window.$ = $;
+
+(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+        $('[data-toggle=tooltip]').tooltip();
+    });
+})(jQuery);
