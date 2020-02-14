35c4667d2b86b2e582b19fbc6a689106e85792e7
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-02-14 12:31:23 +0100

    Adds missing flashes and ability to remove an application

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -6,6 +6,7 @@ use App\Entity\Application;
 use App\Form\ApplicationType;
 use App\Repository\ApplicationRepositoryInterface;
 use App\Service\ApplicationKeyGenerator;
+use SchoolIT\CommonBundle\Form\ConfirmType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -43,8 +44,8 @@ class ApplicationController extends AbstractController {
 
         if($form->isSubmitted() && $form->isValid()) {
             $application->setApiKey($keyGenerator->generateApiKey());
-
             $this->repository->persist($application);
+            $this->addFlash('success', 'applications.add.success');
 
             return $this->redirectToRoute('applications');
         }
@@ -63,6 +64,7 @@ class ApplicationController extends AbstractController {
 
         if($form->isSubmitted() && $form->isValid()) {
             $this->repository->persist($application);
+            $this->addFlash('success', 'applications.edit.success');
 
             return $this->redirectToRoute('applications');
         }
@@ -76,7 +78,25 @@ class ApplicationController extends AbstractController {
     /**
      * @Route("/{id}/remove", name="remove_application")
      */
-    public function remove() {
+    public function remove(Application $application, Request $request) {
+        $form = $this->createForm(ConfirmType::class, null, [
+            'message' => 'applications.remove.confirm',
+            'message_parameters' => [
+                '%name%' => $application->getName()
+            ]
+        ]);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->repository->remove($application);
+            $this->addFlash('success', 'applications.remove.success');
+
+            return $this->redirectToRoute('applications');
+        }
+
+        return $this->render('applications/remove.html.twig', [
+            'application' => $application,
+            'form' => $form->createView()
+        ]);
     }
 }
