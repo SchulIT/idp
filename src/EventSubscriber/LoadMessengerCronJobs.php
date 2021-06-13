c84c3a30bce09a0f60d75304bd10c00adbd0cc90
Marcel Marnitz <marcelmarnitz@googlemail.com>
2021-06-13 19:42:45 +0200

    Create messenger:consume cronjob

---
+++ src/EventSubscriber/LoadMessengerCronJobs.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Shapecode\Bundle\CronBundle\Event\LoadJobsEvent;
+use Shapecode\Bundle\CronBundle\Model\CronJobMetadata;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LoadMessengerCronJobs implements EventSubscriberInterface {
+
+    public function onLoadJobs(LoadJobsEvent $event) {
+        $event->addJob(new CronJobMetadata('*/1 * * * *', 'messenger:consume', '-vv --time-limit=20 --limit=25'));
+        $event->addJob(new CronJobMetadata('*/1 * * * *', 'messenger:consume', 'mail -vv --time-limit=20 --limit=25'));
+        $event->addJob(new CronJobMetadata('*/1 * * * *', 'messenger:consume', 'provision -vv --time-limit=20 --limit=25'));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents() {
+        return [
+            LoadJobsEvent::NAME => 'onLoadJobs',
+        ];
+    }
+}
