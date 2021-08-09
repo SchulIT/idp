00bfa7f542e566c134539bba3073ebe9e8651ea6
Marcel Marnitz <marcelmarnitz@googlemail.com>
2021-08-09 22:03:33 +0200

    Add RemovePcntlEventSubscriberPass() to kernel (was removed in previous commits)

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\DependencyInjection\Compiler\RemovePcntlEventSubscriberPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
@@ -35,5 +37,9 @@ class Kernel extends BaseKernel
         } else {
             $routes->import('../config/{routes}.php');
         }
+    }
+
+    protected function build(ContainerBuilder $container) {
+        $container->addCompilerPass(new RemovePcntlEventSubscriberPass());
     }
 }
