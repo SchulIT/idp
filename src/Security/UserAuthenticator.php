641274e4502060c5eea684a27abd10e9cd46bbfa
Marcel Marnitz <marcelmarnitz@googlemail.com>
2021-08-10 22:34:55 +0200

    Fixes "remember me" not working

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -21,6 +21,7 @@ use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationExc
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -126,6 +127,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator {
             ),
             [
                 new CsrfTokenBadge('authenticate', $csrfToken),
+                new RememberMeBadge(),
                 new CachePasswordBadge($password, $this->hasher)
             ]
         );
