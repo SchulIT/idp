8b5d927fc06e064e044af485a26cc1d6d9e330bd
Marcel Marnitz <marcelmarnitz@googlemail.com>
2021-12-31 13:38:36 +0100

    Fix deprecation

--- src/EventSubscriber/HandleSamlRequestSubscriber.php
+++ src/EventSubscriber/HandleSamlRequestSubscriber.php
@@ -44,7 +44,7 @@ class HandleSamlRequestSubscriber implements EventSubscriberInterface {
             $event->stopPropagation(); // stop other events from possibly redirecting
         }
 
-        if($token === null || !$token->isAuthenticated() || $token instanceof AnonymousToken || $token instanceof TwoFactorToken || $route === 'idp_saml') {
+        if($token === null || $token->getUser() === null || $token instanceof TwoFactorToken || $route === 'idp_saml') {
             // prevent loops
             return;
         }
