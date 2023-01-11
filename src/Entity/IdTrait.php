0d29631d2fa027d34cd5382de5504769834f07ca
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-01-11 23:39:27 +0100

    Initialize value

--- src/Entity/IdTrait.php
+++ src/Entity/IdTrait.php
@@ -11,7 +11,7 @@ trait IdTrait {
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer', options: ['unsigned' => true])]
     #[Serializer\Exclude]
-    private ?int $id;
+    private ?int $id = null;
 
     public function getId(): ?int {
         return $this->id;
