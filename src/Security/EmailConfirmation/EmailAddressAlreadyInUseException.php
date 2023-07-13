ba63d358ceb0479b56d23db280adb1648fe368a1
Marcel Marnitz <marcelmarnitz@googlemail.com>
2023-07-13 09:38:12 +0200

    Fixes an error from PHPStan

--- src/Security/EmailConfirmation/EmailAddressAlreadyInUseException.php
+++ src/Security/EmailConfirmation/EmailAddressAlreadyInUseException.php
@@ -3,6 +3,7 @@
 namespace App\Security\EmailConfirmation;
 
 use Exception;
+use Throwable;
 
 class EmailAddressAlreadyInUseException extends Exception {
     public function __construct(string $email, int $code = 0, ?Throwable $previous = null) {
